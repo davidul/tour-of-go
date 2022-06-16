763907d5602f8d4f4e6711d416abee8de684d6fd
David Ulicny <ulicny.david@gmail.com>
2022-06-16 15:40:17 +0200

    pointers

--- 07-pointers/pointer.go
+++ 07-pointers/pointer.go
@@ -6,4 +6,14 @@ func main() {
 
 	var x = 1
 	fmt.Println(x, &x)
+
+	//map is a reference type, always passed as a pointer
+	m := make(map[string]string, 2)
+	key := "key"
+	p(m, &key)
+	println(m[key])
+}
+
+func p(m map[string]string, key *string) {
+	m[*key] = "value"
 }
