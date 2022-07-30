a6d2cc85fd7c3f6ac2e3833647e2529de1990797
David Ulicny <ulicny.david@gmail.com>
2022-07-30 07:45:59 +0200

    pointers

--- 08-struct/struct.go
+++ 08-struct/struct.go
@@ -2,17 +2,21 @@ package main
 
 import "fmt"
 
+type person struct {
+	firstName string
+	lastName  string
+	age       int
+}
+
 func main() {
 
-	type person struct {
-		firstName string
-		lastName string
-		age int
-	}
-
 	var david = person{firstName: "David", lastName: "Ulicny", age: 45}
+	var davidP = &person{firstName: "David", lastName: "Ulicny", age: 45}
+	f(david)
+	f1(davidP) //via pointer it mutates the struct
 	fmt.Println(david)
-	
+	fmt.Println(davidP)
+
 	var bob = person{"Bob", "Cat", 12}
 	fmt.Println(bob)
 
@@ -21,4 +25,12 @@ func main() {
 	alice.lastName = "Wonderland"
 	alice.age = 18
 	fmt.Println(alice)
+}
+
+func f(p person) {
+	p.firstName = "Daniel"
+}
+
+func f1(p *person) {
+	p.firstName = "Daniel"
 }
